688b8d1f894f01f1b0d8967da1247a9a55a9629f
Noah Breit <nhbreit2@illinois.edu>
2024-02-29 00:47:38 -0600

    Revert "updated var_mux.sv . finished cpu datapath --minus NZP logic"
    This reverts commit c936cb342441879ed343a6a0dcbc8e9d990e31fe.

--- lab5/ece385_lab5.srcs/sources_1/new/var_mux.sv
+++ lab5/ece385_lab5.srcs/sources_1/new/var_mux.sv
@@ -20,15 +20,23 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 module var_mux #(parameter  WIDTH = 16, 
-                 parameter  CHANNELS = 4,
-                 localparam SEL = $clog2(CHANNELS)
+                 parameter  CHANNELS = 4
 ) (
     input logic     [(WIDTH - 1) : 0] din [CHANNELS],
-    input logic     [(SEL - 1) : 0] sel,
+    input logic     [(CHANNELS - 1) : 0] sel,           // ONE HOT
     
     output logic    [(WIDTH - 1) : 0] out
 );
 
-assign out = din[sel];
+always_comb
+begin
+    unique case (sel)
+        0: out = din[0];
+        2: out = din[1];
+        4: out = din[2];
+        8: out = din[3];
+        default: out = 'X;
+    endcase
+end
 
 endmodule
