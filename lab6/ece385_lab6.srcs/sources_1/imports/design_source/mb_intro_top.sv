ccbc138d75efb80b085ddc941e5e30ee92778ea0
Noah Breit <nhbreit2@illinois.edu>
2024-03-02 17:33:26 -0600

    completed vitis software setup. need to add GPIO port2 to microblaze block and adjust software accordingly

--- lab6/ece385_lab6.srcs/sources_1/imports/design_source/mb_intro_top.sv
+++ lab6/ece385_lab6.srcs/sources_1/imports/design_source/mb_intro_top.sv
@@ -13,6 +13,7 @@ module mb_intro_top
    (input  logic clk,
     output logic [15:0] led,
     input  logic [3:0] btn,
+    input  logic [15:0] sw,
     output logic uart_rxd,
     input  logic uart_txd);
 
@@ -24,13 +25,19 @@ module mb_intro_top
 //        .uart_rtl_0_txd(uart_rxd)); //RXD = Received Data, and TXD = Transmitted Data, but whether data is transmitted or received depeneds on the
 //                                    //perspective. Here, the TXD port means transmitted by the FPGA (but received by the Urbana Board's UART chip)
 
+    mb_block_wrapper mb_block_wrapper_i
+        (.clk_100MHz(clk),
+         .gpio_rtl_0_tri_o(led[0]),
+         .reset_rtl_0(~btn[0]),      //Note the inversion of the reset button. Buttons are active low, but the MicroBlaze reset is active high
+         .uart_rtl_0_rxd(uart_txd),  //Note the switcheroo between RTX and TXD. This is a common source of confusion in embedded development
+         .uart_rtl_0_txd(uart_rxd)); //RXD = Received Data, and TXD = Transmitted Data, but whether data is transmitted or received depeneds on the
+                                     //perspective. Here, the TXD port means transmitted by the FPGA (but received by the Urbana Board's UART chip)
     
-  mb_block_wrapper mb_block_wrapper_i
-       (.clk_100MHz(clk),
-        .gpio_rtl_0_tri_o(led[0]),
-        .reset_rtl_0(~btn[0]),      //Note the inversion of the reset button. Buttons are active low, but the MicroBlaze reset is active high
-        .uart_rtl_0_rxd(uart_txd),  //Note the switcheroo between RTX and TXD. This is a common source of confusion in embedded development
-        .uart_rtl_0_txd(uart_rxd)); //RXD = Received Data, and TXD = Transmitted Data, but whether data is transmitted or received depeneds on the
-                                    //perspective. Here, the TXD port means transmitted by the FPGA (but received by the Urbana Board's UART chip)
-
+    sync_flop sw_sync [15:0] (
+        .clk	(clk),
+        .d		(sw),
+    
+        .q		(sw_s)
+    );
+    
 endmodule
