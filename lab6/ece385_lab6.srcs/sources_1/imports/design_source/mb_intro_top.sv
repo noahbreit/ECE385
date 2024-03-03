4fb70808cfbe90401d608a41dbe85f63a52ad0bb
Noah Breit <nhbreit2@illinois.edu>
2024-03-02 18:20:20 -0600

    added second gpio channel to hardware impl. tied this to smt switches.

--- lab6/ece385_lab6.srcs/sources_1/imports/design_source/mb_intro_top.sv
+++ lab6/ece385_lab6.srcs/sources_1/imports/design_source/mb_intro_top.sv
@@ -16,7 +16,11 @@ module mb_intro_top
     input  logic [15:0] sw,
     output logic uart_rxd,
     input  logic uart_txd);
+    
+    // LOCALS
+    logic [15:0] sw_s;      // DEBOUNCED
 
+    // MICROBLAZE SoC Verilog Wrapper
 //  mb_block mb_block_i
 //       (.clk_100MHz(clk),
 //        .gpio_rtl_0_tri_o(led[0]),
@@ -25,14 +29,17 @@ module mb_intro_top
 //        .uart_rtl_0_txd(uart_rxd)); //RXD = Received Data, and TXD = Transmitted Data, but whether data is transmitted or received depeneds on the
 //                                    //perspective. Here, the TXD port means transmitted by the FPGA (but received by the Urbana Board's UART chip)
 
+    // MICROBLAZE SoC SysVerilog Wrapper
     mb_block_wrapper mb_block_wrapper_i
         (.clk_100MHz(clk),
          .gpio_rtl_0_tri_o(led[0]),
+         .gpio_rtl_1_tri_i(sw_s),    // WEEK1 LAB -- ADDED SW INPUT TO MICROBLAZE
          .reset_rtl_0(~btn[0]),      //Note the inversion of the reset button. Buttons are active low, but the MicroBlaze reset is active high
          .uart_rtl_0_rxd(uart_txd),  //Note the switcheroo between RTX and TXD. This is a common source of confusion in embedded development
          .uart_rtl_0_txd(uart_rxd)); //RXD = Received Data, and TXD = Transmitted Data, but whether data is transmitted or received depeneds on the
                                      //perspective. Here, the TXD port means transmitted by the FPGA (but received by the Urbana Board's UART chip)
     
+    // SWITCH INPUT DEBOUNCE
     sync_flop sw_sync [15:0] (
         .clk	(clk),
         .d		(sw),
