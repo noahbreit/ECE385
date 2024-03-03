d76f73db7728b3e3a89c9976201387f16e6ae3be
Noah Breit <nhbreit2@illinois.edu>
2024-03-02 18:28:57 -0600

    increased GPIO CH0 bitwidth to 16 and tied to all 16 output LEDs

--- lab6/ece385_lab6.srcs/sources_1/imports/design_source/mb_intro_top.sv
+++ lab6/ece385_lab6.srcs/sources_1/imports/design_source/mb_intro_top.sv
@@ -20,7 +20,7 @@ module mb_intro_top
     // LOCALS
     logic [15:0] sw_s;      // DEBOUNCED
 
-    // MICROBLAZE SoC Verilog Wrapper
+    // MICROBLAZE SoC Block Wrapper
 //  mb_block mb_block_i
 //       (.clk_100MHz(clk),
 //        .gpio_rtl_0_tri_o(led[0]),
@@ -29,10 +29,10 @@ module mb_intro_top
 //        .uart_rtl_0_txd(uart_rxd)); //RXD = Received Data, and TXD = Transmitted Data, but whether data is transmitted or received depeneds on the
 //                                    //perspective. Here, the TXD port means transmitted by the FPGA (but received by the Urbana Board's UART chip)
 
-    // MICROBLAZE SoC SysVerilog Wrapper
+    // MICROBLAZE SoC Verilog Wrapper
     mb_block_wrapper mb_block_wrapper_i
         (.clk_100MHz(clk),
-         .gpio_rtl_0_tri_o(led[0]),
+         .gpio_rtl_0_tri_o(led),     // WEEK1 LAB -- INCREASED GPIO CH1 Bitwidth to 16 and tied to output smt LEDs
          .gpio_rtl_1_tri_i(sw_s),    // WEEK1 LAB -- ADDED SW INPUT TO MICROBLAZE
          .reset_rtl_0(~btn[0]),      //Note the inversion of the reset button. Buttons are active low, but the MicroBlaze reset is active high
          .uart_rtl_0_rxd(uart_txd),  //Note the switcheroo between RTX and TXD. This is a common source of confusion in embedded development
