b09416f12e86b4921fa89b93695a22180b518fcc
Noah Breit <nhbreit2@illinois.edu>
2024-03-08 10:17:07 -0600

    updated vivado hardware project to current specs

--- lab6/ece385_lab6.srcs/sources_1/imports/design_source/mb_intro_top.sv
+++ lab6/ece385_lab6.srcs/sources_1/imports/design_source/mb_intro_top.sv
@@ -18,7 +18,7 @@ module mb_intro_top
     input  logic uart_txd);
     
     // LOCALS
-    logic [15:0] sw_s;      // DEBOUNCED
+//    logic [15:0] sw_s;      // DEBOUNCED
 
     // MICROBLAZE SoC Block Wrapper
 //  mb_block mb_block_i
@@ -33,18 +33,19 @@ module mb_intro_top
     mb_block_wrapper mb_block_wrapper_i
         (.clk_100MHz(clk),
          .gpio_rtl_0_tri_o(led),     // WEEK1 LAB -- INCREASED GPIO CH1 Bitwidth to 16 and tied to output smt LEDs
-         .gpio_rtl_1_tri_i(sw_s),    // WEEK1 LAB -- ADDED SW INPUT TO MICROBLAZE
+         .gpio_rtl_1_tri_i(sw),      // WEEK1 LAB -- ADDED SW INPUT TO MICROBLAZE
+         .gpio_rtl_2_tri_i(btn[1]),  // WEEK1 LAB -- ADDED ACCUMULATOR BUTTON INPUT TO MICROBLAZE
          .reset_rtl_0(~btn[0]),      //Note the inversion of the reset button. Buttons are active low, but the MicroBlaze reset is active high
          .uart_rtl_0_rxd(uart_txd),  //Note the switcheroo between RTX and TXD. This is a common source of confusion in embedded development
          .uart_rtl_0_txd(uart_rxd)); //RXD = Received Data, and TXD = Transmitted Data, but whether data is transmitted or received depeneds on the
                                      //perspective. Here, the TXD port means transmitted by the FPGA (but received by the Urbana Board's UART chip)
     
     // SWITCH INPUT DEBOUNCE
-    sync_flop sw_sync [15:0] (
-        .clk	(clk),
-        .d		(sw),
+//    sync_flop sw_sync [15:0] (
+//        .clk	(clk),
+//        .d		(sw),
     
-        .q		(sw_s)
-    );
+//        .q		(sw_s)
+//    );
     
 endmodule
