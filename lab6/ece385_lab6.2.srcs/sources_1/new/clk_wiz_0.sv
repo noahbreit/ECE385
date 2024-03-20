5a63709d33188d61be3c38e68b9c35679625adb2
Noah Breit <nhbreit2@illinois.edu>
2024-03-20 11:03:10 -0500

    created clk_wizard and vga_to_hdmi IP block designs with corr sysverilog wrappers. TODO: finish wrappers

---
+++ lab6/ece385_lab6.2.srcs/sources_1/new/clk_wiz_0.sv
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/20/2024 11:00:20 AM
+// Design Name: 
+// Module Name: clk_wiz_0
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module clk_wiz_0(
+    // TODO!
+    );
+    
+    clk_wiz_block_wrapper clk_wiz_block_wrapper (.*);
+    
+endmodule
---
+++ lab6/ece385_lab6.2.srcs/sources_1/new/hdmi_tx_0.sv
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/20/2024 10:54:55 AM
+// Design Name: 
+// Module Name: hdmi_tx_0
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module hdmi_tx_0(
+    // TODO!
+    );
+    
+    vga_to_hdmi_block_wrapper vga_to_hdmi_block_wrapper (.*);
+    
+endmodule
