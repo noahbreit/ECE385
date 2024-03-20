7434496998fb1d5346740d3f5e8f3477a47c6d7b
Noah Breit <nhbreit2@illinois.edu>
2024-03-19 23:39:13 -0500

    added base project + configured microblaze block and wrapper

---
+++ lab6/ece385_lab6.2.srcs/sources_1/new/mb_usb.sv
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/19/2024 10:44:26 PM
+// Design Name: 
+// Module Name: mb_usb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module mb_usb(
+    // TODO!
+    );
+    
+    mb_block_wrapper mb_block_wrapper (.*);
+    
+endmodule
